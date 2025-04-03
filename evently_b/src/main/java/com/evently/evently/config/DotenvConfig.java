e2a720dac19bc05428ab63b872bd9f9d141c606a
Dieg0Ch4ves <diegoaraujo200570@gmail.com>
2025-04-03 19:47:52 -0400

    refactor: alterando configuração de Dotenv para rodar restes no Github Actions

--- evently_b/src/main/java/com/evently/evently/config/DotenvConfig.java
+++ evently_b/src/main/java/com/evently/evently/config/DotenvConfig.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.Configuration;
 public class DotenvConfig {
 
     static {
-        Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            if (System.getenv(entry.getKey()) == null) {
+                System.setProperty(entry.getKey(), entry.getValue());
+            }
+        });
     }
 }
